26882da2b2b6ce4beec9d425ded8f578dbcc5abd
Sahaj Dureja <85799644+sahax1709@users.noreply.github.com>
2026-03-03 20:22:12 +0530

    Delete isl-detector directory

--- isl-detector/render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Install Python dependencies
-pip install -r requirements.txt
-
-# Install system dependencies for OpenCV/MediaPipe
-apt-get update && apt-get install -y libgl1
